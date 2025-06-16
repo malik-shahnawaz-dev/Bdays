6ebee3b237f5f48415c858459d87f1e6cdaceaeb
shahnawaz <malik.shahnawaz2116@gmail.com>
2025-06-16 11:02:42 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ChangeTitle from './useState/changeTitle/ChangeTitle'
-import Circuits from './shortCircuits/Circuits'
 import Birthdays from './bdays/Birthdays'
 
 const App = () => {
